5e1857ad2125be7a48c6d80d50c3186e4392b770
Silika-PDU <silika.pdu@gmail.com>
2026-03-31 10:18:48 +0200

    problem set 1 bank

--- deep/deep.py
+++ deep/deep.py
@@ -3,7 +3,6 @@ question = input("What is the answer to the Great Question of Life, the Universe
 optimized = question.strip().lower()
 if optimized == "42" or optimized == "forty-two" or optimized == "forty two":
     print("Yes")
-elif question == "69":
-    print("nice.")
+
 else:
     print("No")
